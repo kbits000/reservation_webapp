e5169f59db46ec6763ee009e60d185d2157feadc
kbits000 <69712340+kbits000@users.noreply.github.com>
2025-07-21 16:16:35 +0300

    add upload file button

--- src/components/ui/admin_pages/reservation_pages/reservations_settings_page.tsx
+++ src/components/ui/admin_pages/reservation_pages/reservations_settings_page.tsx
@@ -8,11 +8,32 @@ import {useState} from "react";
 import { InputNumber, TimePicker, Space, Typography } from "antd";
 const { RangePicker } = TimePicker;
 const { Text } = Typography;
+import { UploadOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import { Button, message, Upload } from 'antd';
+
 
 export default function ReservationsSettingsPage() {
     const [advanceBookingDaysValue, setAdvanceBookingDaysValue] = useState<number | string | null>(null);
     const [numberOfAllowedReservationsPerDay, setNumberOfAllowedReservationsPerDay] = useState<number | string | null>(null);
     const [timePeriodForSunday, setTimePeriodForSunday] = useState<[Dayjs, Dayjs] | [null, null]>([null, null]);
+
+
+    const props: UploadProps = {
+        name: 'file',
+        action: '/api/fileupload',
+        onChange(info) {
+            console.log('info: ', info);
+            if (info.file.status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (info.file.status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully`);
+            } else if (info.file.status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+    };
 
     return (
         <div className='pt-4'>
@@ -117,6 +138,11 @@ export default function ReservationsSettingsPage() {
                         </Space>
                     </Space>
                 </div>
+            </div>
+            <div className='pb-2'>
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
             </div>
         </div>
     );
